296ed9d8bd15376dc7294b10f42c2d6f8e5180eb
Csete Gábor <csgabor88@gmail.com>
2019-09-12 12:35:25 +0200

    first 4 exercises

---
+++ week-01/day-4/hello-others.ts
@@ -0,0 +1,11 @@
+'use strict';
+
+// Greet 3 of your classmates with this program, in three separate lines like:
+//
+// Hello, Esther!
+// Hello, Mary!
+// Hello, Joe!
+
+console.log('Hello, Kriszti!');
+console.log('Hello, Misi!');
+console.log('Hello, Daniel!');
---
+++ week-01/day-4/humpty-dumpty.ts
@@ -0,0 +1,19 @@
+'use strict';
+
+/* Modify this program to console.log Humpty Dumpty riddle correctly
+
+console.log('All the king\'s horses and all the king\'s men');
+console.log('Humpty Dumpty had a great fall.');
+console.log('Humpty Dumpty sat on a wall,');
+console.log('Couldn\'t put Humpty together again.');
+
+*/
+
+console.log('Humpty Dumpty');
+console.log('sat on a wall,');
+console.log('Humpty Dumpty');
+console.log('had a great fall.');
+console.log('All the king\'s horses');
+console.log('and all the king\'s men');
+console.log('Couldn\'t put Humpty');
+console.log('together again.');
