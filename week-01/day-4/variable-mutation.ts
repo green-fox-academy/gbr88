5c0fee6a0c1746ddd870236c687721b1deb7712a
Csete Gábor <csgabor88@gmail.com>
2019-09-13 09:40:08 +0200

    finished and used shorten formats

--- week-01/day-4/variable-mutation.ts
+++ week-01/day-4/variable-mutation.ts
@@ -41,22 +41,18 @@ let g2: number = 200;
 // tell if the double of g2 is bigger than g1 (pras a boolean)
 console.log(g2*2 > g1);
 
-/*
+
 let h: number = 1357988018575474;
 // tell if 11 is a divisor of h (as a boolean)
-h = h % 11;
-h == 0? console.log(true): console.log(false);
+console.log(h % 11 === 0);
 
 
 let i1: number = 10;
 let i2: number = 3;
 // tell if i1 is higher than i2 squared and smaller than i2 cubed (as a boolean)
-if (i1 > Math.pow(i2, 2) && i1 < Math.pow(i2, 3)){
-  console.log(true);
-} console.log(false);
+console.log(i1 > Math.pow(i2, 2) && i1 < Math.pow(i2, 3))
 
 
 let j: number = 1521;
 // tell if j is divisible by 3 or 5 (as a boolean)
-
-*/
+console.log(j % 3 === 0 || j % 5 === 0);
