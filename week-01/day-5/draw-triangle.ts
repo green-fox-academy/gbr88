1170d8fdde902ef8388420d9080201766b9946b2
Csete Gábor <csgabor88@gmail.com>
2019-09-13 16:44:15 +0200

    make dem stars appear boi!

---
+++ week-01/day-5/draw-triangle.ts
@@ -0,0 +1,19 @@
+'use strict';
+
+let lineCount: number = 4;
+// Write a program that draws a triangle like this:
+//
+// *
+// **
+// ***
+// ****
+//
+// The triangle should have as many lines as lineCount is
+
+for (let i: number = 1; i <= lineCount; i++) {
+  let stars: string = ""
+  for (let j: number = 1; j <= i; j++) {
+  stars += "*"
+  }
+  console.log(stars)
+}
