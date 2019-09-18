6849ef2c0d3ce4bd50d9407aed1068b7ba09381c
Csete Gábor <csgabor88@gmail.com>
2019-09-18 09:27:50 +0200

    progressing still not finished

--- week-02/day-2/map-introduction-1.ts
+++ week-02/day-2/map-introduction-1.ts
@@ -1,29 +1,6 @@
 'use strict';
 
-/*
-We are going to play with maps. Feel free to use the built-in methods where possible.
-
-
-
-
-
-
-
-
-
-
-Add value D with the key 68
-
-Print how many key-value pairs are in the map
-
-Print the value that is associated with key 99
-
-Remove the key-value pair where with key 97
-
-Print whether there is an associated value with key 100 or not
-
-Remove all the key-value pairs
-*/
+//We are going to play with maps. Feel free to use the built-in methods where possible.
 
 //Create an empty map where the keys are integers and the values are characters
 interface maps {
@@ -57,3 +34,19 @@ maps[67] = 'C';
 console.log(Object.keys(maps));
 
 //Print all the values
+let values: string[] = Object.keys(maps).map(e => maps[e]);
+console.log(values);
+
+//Add value D with the key 68
+maps[68] = 'D';
+
+//Print how many key-value pairs are in the map
+console.log(Object.keys(maps).length);
+
+//Print the value that is associated with key 99
+
+//Remove the key-value pair where with key 97
+
+//Print whether there is an associated value with key 100 or not
+
+//Remove all the key-value pairs
