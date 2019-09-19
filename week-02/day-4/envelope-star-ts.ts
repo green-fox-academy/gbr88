f24c1c8d0fb37361d569fd94fcfd6337b0189f8c
Csete Gábor <csgabor88@gmail.com>
2019-09-19 16:35:19 +0200

    not finished and not working properly

---
+++ week-02/day-4/envelope-star-ts.ts
@@ -0,0 +1,23 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+
+function drawLine(mX: number, mY: number, lX: number, lY: number, color: string) {
+  ctx.strokeStyle = color;
+  ctx.beginPath();
+  ctx.moveTo(mX, mY);
+  ctx.lineTo(lX, lY);
+  ctx.stroke();
+}
+
+let mX: number = canvas.width / 2;
+let mY: number = canvas.height;
+let lX: number = canvas.width / 2;
+let lY: number = canvas.height / 2;
+let steps: number = canvas.width / 20;
+
+for (let i: number = 1; i <= canvas.width / 50; i++) {
+  drawLine(mX, mY, lX + steps, lY, 'green');
+  mY -= steps; lX += steps;
+}
