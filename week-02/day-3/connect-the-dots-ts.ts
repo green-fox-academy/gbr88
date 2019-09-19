38a35ed79825c1485d5ae9107c6aee528f0138f8
Csete Gábor <csgabor88@gmail.com>
2019-09-19 11:59:41 +0200

    finished

--- week-02/day-3/connect-the-dots-ts.ts
+++ week-02/day-3/connect-the-dots-ts.ts
@@ -12,4 +12,21 @@ const ctx = canvas.getContext('2d');
 // Connect these: [[50, 100], [70, 70], [80, 90], [90, 90], [100, 70],
 // [120, 100], [85, 130], [50, 100]]
 
-let cords: number[][] = [[10, 10], [290,  10], [290, 290], [10, 290]];
+let cords1: number[][] = [[10, 10], [290, 10], [290, 290], [10, 290]];
+let cords2: number[][] = [[50, 100], [70, 70], [80, 90], [90, 90], [100, 70], [120, 100], [85, 130], [50, 100]];
+
+function connectDots(cords: number[][]) {
+  for (let i: number = 0; i <= cords.length; i++) {
+    ctx.strokeStyle = 'Green';
+    if (i < cords.length) {
+      ctx.lineTo(cords[i][0], cords[i][1]);
+      ctx.stroke();
+    } else if (i === cords.length) {
+      ctx.lineTo(cords[0][0], cords[0][1]);
+      ctx.stroke();
+    }
+  } ctx.beginPath(); 
+}
+
+connectDots(cords1);
+connectDots(cords2);
