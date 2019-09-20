670c6990db881a9cccd4a6ced0606dd5f9b23d5e
Csete Gábor <csgabor88@gmail.com>
2019-09-20 14:58:18 +0200

    fixed movement now its works properly

--- week-02/day-5/super-hexagon-ts.ts
+++ week-02/day-5/super-hexagon-ts.ts
@@ -3,43 +3,38 @@
 const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d');
 
-let size: number = 20;
-let startX: number = canvas.width / 2;
-let startY: number = 0;
-let end: number = 0;
 
-function drawHex(size: number, startX: number, startY: number, end: number) {
-
+function drawHex(size: number, startX: number, startY: number) {
 
   ctx.beginPath();
   ctx.moveTo(startX - size / 2, startY);
-  ctx.lineTo((startX - size / 2) + size, end);
+  ctx.lineTo((startX - size / 2) + size, startY);
   ctx.stroke();
 
   ctx.beginPath();
-  ctx.moveTo((startX - size / 2) + size, end);
-  ctx.lineTo((startX - size / 2) + size + size / 2, end + size * ((Math.sqrt(3)) / 2));
+  ctx.moveTo((startX - size / 2) + size, startY);
+  ctx.lineTo((startX - size / 2) + size + size / 2, startY + size * ((Math.sqrt(3)) / 2));
   ctx.stroke();
 
   ctx.beginPath();
-  ctx.moveTo((startX - size / 2) + size + size / 2, end + size * ((Math.sqrt(3)) / 2));
-  ctx.lineTo((startX - size / 2) + size, end + size * ((Math.sqrt(3)) / 2) + end + size * ((Math.sqrt(3)) / 2));
+  ctx.moveTo((startX - size / 2) + size + size / 2, startY + size * ((Math.sqrt(3)) / 2));
+  ctx.lineTo((startX - size / 2) + size, size * ((Math.sqrt(3)) / 2) + startY + size * ((Math.sqrt(3)) / 2));
   ctx.stroke();
 
   ctx.beginPath();
-  ctx.moveTo((startX - size / 2) + size, end + size * ((Math.sqrt(3)) / 2) + end + size * ((Math.sqrt(3)) / 2));
-  ctx.lineTo((startX - size / 2), startY + end + size * ((Math.sqrt(3)) / 2) + end + size * ((Math.sqrt(3)) / 2));
+  ctx.moveTo((startX - size / 2) + size, size * ((Math.sqrt(3)) / 2) + startY + size * ((Math.sqrt(3)) / 2));
+  ctx.lineTo((startX - size / 2), startY + size * ((Math.sqrt(3)) / 2) + size * ((Math.sqrt(3)) / 2));
   ctx.stroke();
 
   ctx.beginPath();
-  ctx.moveTo((startX - size / 2), startY + end + size * ((Math.sqrt(3)) / 2) + end + size * ((Math.sqrt(3)) / 2));
-  ctx.lineTo((startX - size / 2) - size / 2, end + size * ((Math.sqrt(3)) / 2));
+  ctx.moveTo((startX - size / 2), startY + size * ((Math.sqrt(3)) / 2) + size * ((Math.sqrt(3)) / 2));
+  ctx.lineTo((startX - size / 2) - size / 2, startY + size * ((Math.sqrt(3)) / 2));
   ctx.stroke();
 
   ctx.beginPath();
-  ctx.moveTo((startX - size / 2) - size / 2, end + size * ((Math.sqrt(3)) / 2));
-  ctx.lineTo(startX - size / 2, startY);
+  ctx.moveTo((startX - size / 2) - size / 2, startY + size * ((Math.sqrt(3)) / 2));
+  ctx.lineTo((startX - size / 2), startY);
   ctx.stroke();
 }
 
-drawHex(size, startX, startY, 0)
+drawHex(100, 200, 20)
