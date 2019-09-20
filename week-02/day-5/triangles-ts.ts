57966cc99ab507c87a6062dd1ec218bcb5abe9ac
Csete Gábor <csgabor88@gmail.com>
2019-09-20 11:31:32 +0200

    1/3 lines working yey

--- week-02/day-5/triangles-ts.ts
+++ week-02/day-5/triangles-ts.ts
@@ -9,3 +9,18 @@ function drawLine(mX: number, mY: number, lX: number, lY: number) {
   ctx.lineTo(lX, lY);
   ctx.stroke();
 }
+
+let steps: number = canvas.width / 20;
+let lines: number = 20;
+
+let startLeftMX: number = 0;
+let startLeftMY: number = canvas.height;
+let startLeftLX: number = canvas.width / 2;
+let startLeftLY: number = 0;
+
+for (let i: number = 1; i <= lines; i++) {
+  drawLine(startLeftMX, startLeftMY, startLeftLX, startLeftLY)
+  startLeftMX += steps;
+  startLeftLX += steps / 2;
+  startLeftLY += steps;
+}
