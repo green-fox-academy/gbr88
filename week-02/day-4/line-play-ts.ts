d5e51c3da4821f999357f963049262f47a8d8ed2
Csete Gábor <csgabor88@gmail.com>
2019-09-22 21:29:39 +0200

    refactored

--- week-02/day-4/line-play-ts.ts
+++ week-02/day-4/line-play-ts.ts
@@ -3,31 +3,49 @@
 const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d');
 
+// constans
+let steps: number = canvas.width / 20; // change value for line density
+let lines: number = canvas.width / steps;
 
+// starting points
+const left = {
+  startMX: 0,
+  startMY: 0,
+  startLX: 0,
+  startLY: canvas.height
+}
+
+const right = {
+  startMX: canvas.width,
+  startMY: 0,
+  startLX: canvas.width,
+  startLY: canvas.height
+}
+
+// draws the lines
+for (let i: number = 1; i <= lines; i++) {
+  drawLeft();
+  drawRight();
+}
+
+// functions
+function drawLeft() {
+  drawLine(left.startMX, left.startMY, left.startLX, left.startLY, 'green');
+  left.startMY += steps;
+  left.startLX += steps;
+}
+
+function drawRight() {
+  drawLine(right.startMX, right.startMY, right.startLX, right.startLY, 'purple');
+  right.startMX -= steps;
+  right.startLY -= steps; 
+}
+
+// helpers
 function drawLine(mX: number, mY: number, lX: number, lY: number, color: string) {
   ctx.strokeStyle = color;
   ctx.beginPath();
   ctx.moveTo(mX, mY);
   ctx.lineTo(lX, lY);
   ctx.stroke();
-}
-
-let steps: number = canvas.width / 20;
-let lines: number = canvas.width / steps;
-
-let leftMX: number = 0;
-let leftMY: number = 0;
-let leftLX: number = 0;
-let leftLY: number = canvas.height;
-
-let rightMX: number = canvas.width;
-let rightMY: number = 0;
-let rightLX: number = canvas.width;
-let rightLY: number = canvas.height;
-
-for (let i: number = 1; i <= lines; i++) {
-  drawLine(leftMX, leftMY, leftLX, leftLY, 'green');
-  leftMY += steps; leftLX += steps;
-  drawLine(rightMX, rightMY, rightLX, rightLY, 'purple');
-  rightMX -= steps; rightLY -= steps; 
 }
