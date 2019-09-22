17f83308f3a7871eaf1bd7a13677921885307bff
Csete Gábor <csgabor88@gmail.com>
2019-09-22 20:45:44 +0200

    refactored. much cleaner code

--- week-02/day-5/triangles-ts.ts
+++ week-02/day-5/triangles-ts.ts
@@ -3,50 +3,66 @@
 const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d');
 
-function drawLine(mX: number, mY: number, lX: number, lY: number) {
+// constans
+const lines: number = 20; // change value for line density
+const steps: number = canvas.width / lines;
+
+// starting points
+const left = {
+  startMX: 0,
+  startMY: canvas.height,
+  startLX: canvas.width / 2,
+  startLY: 0
+}
+
+const right = {
+  startMX: canvas.width,
+  startMY: canvas.height,
+  startLX: canvas.width / 2,
+  startLY: 0
+}
+
+const line = {
+  startMX: 0,
+  startMY: canvas.height,
+  startLX: canvas.width,
+  startLY: canvas.height
+}
+
+// draws the pyramid of triangles
+for (let i: number = 1; i <= lines; i++) {
+  drawLeft();
+  drawLine();
+  drawRight();
+}
+
+// functions
+function drawLeft() {
+  drawSingleLine(left.startMX, left.startMY, left.startLX, left.startLY)
+  left.startMX += steps;
+  left.startLX += steps / 2;
+  left.startLY += steps;
+}
+
+function drawRight() {
+  drawSingleLine(right.startMX, right.startMY, right.startLX, right.startLY)
+  right.startMX -= steps;
+  right.startLX -= steps / 2;
+  right.startLY += steps;
+}
+
+function drawLine() {
+  drawSingleLine(line.startMX, line.startMY, line.startLX, line.startLY)
+  line.startMX += steps / 2;
+  line.startMY -= steps;
+  line.startLX -= steps / 2;
+  line.startLY -= steps;
+}
+
+// helpers
+function drawSingleLine(mX: number, mY: number, lX: number, lY: number) {
   ctx.beginPath();
   ctx.moveTo(mX, mY);
   ctx.lineTo(lX, lY);
   ctx.stroke();
-}
-
-let lines: number = 20;
-let steps: number = canvas.width / lines;
-
-
-let startLeftMX: number = 0;
-let startLeftMY: number = canvas.height;
-let startLeftLX: number = canvas.width / 2;
-let startLeftLY: number = 0;
-
-for (let i: number = 1; i <= lines; i++) {
-  drawLine(startLeftMX, startLeftMY, startLeftLX, startLeftLY)
-  startLeftMX += steps;
-  startLeftLX += steps / 2;
-  startLeftLY += steps;
-}
-
-let startRightMX: number = canvas.width;
-let startRightMY: number = canvas.height;
-let startRightLX: number = canvas.width / 2;
-let startRightLY: number = 0;
-
-for (let i: number = 1; i <= lines; i++) {
-  drawLine(startRightMX, startRightMY, startRightLX, startRightLY)
-  startRightMX -= steps;
-  startRightLX -= steps / 2;
-  startRightLY += steps;
-}
-
-let startLineMX: number = 0;
-let startLineMY: number = canvas.height;
-let startLineLX: number = canvas.width;
-let startLineLY: number = canvas.height;
-
-for (let i: number = 1; i <= lines; i++) {
-  drawLine(startLineMX, startLineMY, startLineLX, startLineLY)
-  startLineMX += steps / 2;
-  startLineMY -= steps;
-  startLineLX -= steps / 2;
-  startLineLY -= steps;
 }
