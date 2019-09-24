6d4e28ec8f69c84ece24e2633b5b8721a5797c03
Csete Gábor <csgabor88@gmail.com>
2019-09-24 13:15:11 +0200

    using try catch

---
+++ week-03/day-2/divide-by-zero.ts
@@ -0,0 +1,22 @@
+'use strict';
+// Create a function that takes a number
+// divides ten with it,
+// and prints the result.
+// It should print 'fail' if the parameter is 0
+
+
+// divide by value that passes
+tenDividedBy(2);
+// divide by value that exception so catch will work
+tenDividedBy(0);
+
+function tenDividedBy(number: number) {
+  try {
+    if (number == 0) {
+      throw 'error'
+    } else console.log(10 / number);
+  }
+  catch (error) {
+    console.log('fail');
+  }
+}
