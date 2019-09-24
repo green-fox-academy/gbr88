0161398b6e7b7861376b9215acb61163838d7f65
Csete Gábor <csgabor88@gmail.com>
2019-09-24 23:13:50 +0200

    homework before matrew

---
+++ week-03/day-3/doable-homework.ts
@@ -0,0 +1,45 @@
+'use strict';
+
+// Create 5 trees
+// Store the data of them in variables in your program
+// for every tree the program should store its'
+// type
+// leaf color
+// age
+// sex
+// you can use just variables, or lists and/or maps
+
+const tree1 = {
+  type: 'paper birch (Betula papyrifera)',
+  leafColor: 'yellow',
+  age: 30,
+  sex: 'monoecious'
+}
+
+const tree2 = {
+  type: 'lodgepole pine (Pinus contorta)',
+  leafColor: 'evergreen',
+  age: 13,
+  sex: 'female'
+}
+
+const tree3 = {
+  type: 'western red cedar (Thuja plicata)',
+  leafColor: 'green',
+  age: 1460,
+  sex: 'monoecious'
+}
+
+const tree4 = {
+  type: 'northern red oak (Quercus rubra)',
+  leafColor: 'red',
+  age: 326,
+  sex: 'monoecious'
+}
+
+const tree5 = {
+  type: 'sugar maple (Acer saccharum)',
+  leafColor: 'red-orange',
+  age: 200,
+  sex: 'male'
+}
