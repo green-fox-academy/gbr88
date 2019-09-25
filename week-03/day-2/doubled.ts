0a69822f8809eb45e0a89d448c1482b4a507e9de
Csete Gábor <csgabor88@gmail.com>
2019-09-25 13:06:44 +0200

    work in progress

--- week-03/day-2/doubled.ts
+++ week-03/day-2/doubled.ts
@@ -2,5 +2,7 @@
 // Create a method that decrypts duplicated-chars.txt
 const fs = require('fs');
 
-let txtContent: string = fs.readFileSync('duplicated-chars', 'utf8');
-let 
+let txtContent: string = fs.readFileSync('duplicated-chars.txt', 'utf8');
+let splittedTxt: string[] = txtContent.split('');
+
+console.log(splittedTxt);
