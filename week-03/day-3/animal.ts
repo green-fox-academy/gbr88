261b5e49a1a9f86c42eb9b76ebbd34c1fca717ec
Csete Gábor <csgabor88@gmail.com>
2019-09-25 15:54:25 +0200

    using default value with constructor

---
+++ week-03/day-3/animal.ts
@@ -0,0 +1,53 @@
+'use strict';
+
+// Create an Animal class
+// Every animal has a hunger value, which is a whole number
+// Every animal has a thirst value, which is a whole number
+// when creating a new animal object these values are created with the default 50 value
+// Every animal can eat() which decreases their hunger by one
+// Every animal can drink() which decreases their thirst by one
+// Every animal can play() which increases both by one
+
+class Animal {
+  private _hunger: number;
+  private _thirst: number;
+  private _number: number;
+
+  constructor(hunger?: number, thirst?: number,) {
+    if (hunger) {
+      this._hunger = hunger;
+    } else {
+      this._hunger = 50;
+    }
+    if (thirst) {
+      this._thirst = thirst;
+    } else {
+      this._thirst = 50;
+    }
+  }
+
+  public eat(): void {
+    this._hunger -= 1;
+  }
+
+  public drink(): void {
+    this._thirst -= 1;
+  }
+
+  public play(): void {
+    this._hunger += 1, this._thirst += 1;
+  }
+
+  public status(): string {
+    return `This animal has hunger: ${this._hunger} and thirst: ${this._thirst}`;
+  }
+
+}
+
+let animal1 = new Animal();
+console.log(animal1.status());
+animal1.play();
+console.log(animal1.status());
+animal1.eat();
+animal1.drink();
+console.log(animal1.status());
