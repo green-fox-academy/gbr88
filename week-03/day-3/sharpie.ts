a46167a7abbdf43f74d37c2a12a527deca19703d
Csete Gábor <csgabor88@gmail.com>
2019-09-25 16:57:37 +0200

    working with class object

---
+++ week-03/day-3/sharpie.ts
@@ -0,0 +1,34 @@
+'use strict';
+// Create Sharpie class
+// We should know about each sharpie their color (which should be a string),
+// width (which will be a floating point number), inkAmount (another floating point number)
+// When creating one, we need to specify the color and the width
+// Every sharpie is created with a default 100 as inkAmount
+// We can use() the sharpie objects
+// which decreases inkAmount
+
+class Sharpie {
+  private _color: string;
+  private _width: number;
+  private _inkAmount: number;
+
+  constructor(color: string, width: number, amount: number = 100) {
+    this._color = color;
+    this._width = width;
+    this._inkAmount = amount;
+
+  }
+
+  public use(): void {
+    this._inkAmount -= 1;
+  }
+
+  public ink(): number {
+    return this._inkAmount;
+  }
+}
+
+let sharpie1 = new Sharpie('black',200);
+console.log(sharpie1.ink());
+sharpie1.use();
+console.log(sharpie1.ink());
