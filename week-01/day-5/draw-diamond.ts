8749d7b018fdfb3902230463c72366cb5a864161
Csete Gábor <csgabor88@gmail.com>
2019-10-08 18:42:16 +0200

    refactoring

--- week-01/day-5/draw-diamond.ts
+++ week-01/day-5/draw-diamond.ts
@@ -27,3 +27,29 @@ for (let i: number = half - 1; i > -half; i--) {
   }
   console.log(stars);
 }
+
+// triangle from top do bottom
+//
+// for (let i: number = 1; i <= lineCount; i++) {
+//   let stars: string = "";
+//   for (let j: number = i; j < lineCount; j++) {
+//     stars += " ";
+//   }
+//   for (let k: number = 0; k < (i * 2) - 1; k++) {
+//     stars += "*";
+//   }
+//   console.log(stars);
+// }
+//
+// triangle from bottom-1 to top
+//
+// for (let i: number = lineCount-1; i <= 1; i--) {
+//   let stars: string = "";
+//   for (let j: number = lineCount-1; j < i; j++) {
+//     stars += " ";
+//   }
+//   for (let k: number = 0; k < (i * 2) - 1; k++) {
+//     stars += "*";
+//   }
+//   console.log(stars);
+// }
