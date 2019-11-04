74e63460f2f115ce0702f6ea208e624ecccc4c98
Csete Gábor <csgabor88@gmail.com>
2019-11-04 13:26:09 +0100

    bad commit

--- week-07/day-4/reddit/app.js
+++ week-07/day-4/reddit/app.js
@@ -5,14 +5,17 @@ const mysql = require('mysql');
 const app = express();
 const PORT = 3000;
 const env = require('dotenv').config();
+const bodyParser = require('body-parser');
 
+app.use(bodyParser.json());
 app.use(express.json());
 
 let conn = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: process.env.DB_DBNAME
+    database: process.env.DB_DBNAME,
+    multipleStatements: true
 });
 
 conn.connect((err) => {
@@ -21,8 +24,74 @@ conn.connect((err) => {
 });
 
 app.get('/hello', function(req, res) {
-    let hi = 'hello'
+    let hi = 'hello world'
     res.send(hi);
+});
+
+app.get('/posts', function(req, res) {
+    req.accepts('application/json');
+    res.status(200);
+    res.header('Content-Type', 'application/json');
+    let sql = `SELECT * FROM posts;`
+    conn.query(sql, function(err, rows) {
+        if (err) {
+            console.log(err.toString());
+            res.status(500).send('Database error');
+            return;
+        }
+        res.send(rows);
+    });
+});
+
+app.post('/posts', function(req, res) {
+    req.accepts('application/json');
+    res.status(200);
+    res.header('Content-Type', 'application/json');
+    let sqlselect = `SELECT * FROM posts WHERE id = (SELECT MAX(id) FROM posts)`;
+    let sqlinsert = `INSERT INTO posts (title, url, timestamp) VALUES ("${req.body.title}", "${req.body.url}", now())`;
+    conn.query(`${sqlinsert}; ${sqlselect}`, function(err, rows) {
+        console.log()
+        if (err) {
+            console.log(err.toString());
+            res.status(500).send('Database error');
+            return;
+        }
+        res.send(rows);
+    });
+});
+
+app.put('/posts/:id/upvote', function(req, res) {
+    req.accepts('application/json');
+    res.status(200);
+    res.header('Content-Type', 'application/json');
+    let sqlselect = `SELECT * FROM posts WHERE id = "${req.params.id}"`;
+    let sqlinsert = `UPDATE posts SET score = score + 1 WHERE id = ${req.params.id}`;
+    conn.query(`${sqlinsert}; ${sqlselect}`, function(err, rows) {
+        console.log()
+        if (err) {
+            console.log(err.toString());
+            res.status(500).send('Database error');
+            return;
+        }
+        res.send(rows);
+    });
+});
+
+app.put('/posts/:id/downvote', function(req, res) {
+    req.accepts('application/json');
+    res.status(200);
+    res.header('Content-Type', 'application/json');
+    let sqlselect = `SELECT * FROM posts WHERE id = "${req.params.id}"`;
+    let sqlinsert = `UPDATE posts SET score = score - 1 WHERE id = ${req.params.id}`;
+    conn.query(`${sqlinsert}; ${sqlselect}`, function(err, rows) {
+        console.log()
+        if (err) {
+            console.log(err.toString());
+            res.status(500).send('Database error');
+            return;
+        }
+        res.send(rows);
+    });
 });
 
 app.listen(PORT);
