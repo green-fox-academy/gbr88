c8b5ab35c1a0c1857cb1f0bd5d8272d31dd50768
Csete Gábor <csgabor88@gmail.com>
2019-11-06 15:45:34 +0100

    buttons with events on conditions

--- week-08/day-3/button-disable/button-disable.js
+++ week-08/day-3/button-disable/button-disable.js
@@ -1,1 +1,47 @@
 'use strict';
+
+let pets = document.getElementById('pets');
+let votes = document.getElementById('votes');
+let options = document.getElementById('options');
+
+options.children[0].disabled = true;
+options.children[1].disabled = true;
+
+
+pets.addEventListener('change', function() {
+    if (pets.children[0].checked == true || pets.children[1].checked == true) {
+        options.children[1].disabled = false;
+    } else {
+        options.children[1].disabled = true;
+    }
+    if (pets.children[2].checked == true && votes.children[1].checked == true) {
+        options.children[1].disabled = false;
+        options.children[0].disabled = true;
+    }
+});
+
+votes.addEventListener('change', function() {
+    if (votes.children[0].checked == true) {
+        options.children[0].disabled = false;
+    }
+    if (votes.children[1].checked == true) {
+        options.children[0].disabled = true;
+    }
+    if (pets.children[2].checked == true && votes.children[0].checked == true) {
+        options.children[0].disabled = false;
+        options.children[1].disabled = true;
+    }
+    if (pets.children[2].checked == true && votes.children[1].checked == true) {
+        options.children[0].disabled = true;
+        options.children[1].disabled = false;
+    }
+
+});
+
+options.addEventListener('click', function(e) {
+    if (pets.children[2].checked == true) {
+        alert('Sigh, we still added you to the cat facts list');
+    } else {
+        alert(`Thank you, you've successfully signed up for cat facts`);
+    }
+});
