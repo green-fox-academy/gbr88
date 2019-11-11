e815e9ec2e95aa7b3dbe51798b9fd331d30d389e
Csete Gábor <csgabor88@gmail.com>
2019-11-11 10:29:54 +0100

    after refactor

--- week-09/day-1/anagram/anagram.js
+++ week-09/day-1/anagram/anagram.js
@@ -1,7 +1,9 @@
 'use strict';
 
 function anagram(string1, string2) {
-    return true;
+    let first = string1.toLowerCase().split('').sort().join('');
+    let second = string2.toLowerCase().split('').sort().join('');
+    return first === second;
 };
 
 module.exports = anagram;
