640b9387fd5e8f7cb38a3d08116aef9f9fd8863c
Csete Gábor <csgabor88@gmail.com>
2019-11-11 10:25:46 +0100

    before refactor

---
+++ week-09/day-1/anagram/anagram.js
@@ -0,0 +1,7 @@
+'use strict';
+
+function anagram(string1, string2) {
+    return true;
+};
+
+module.exports = anagram;
---
+++ week-09/day-1/anagram/test.js
@@ -0,0 +1,12 @@
+'use strict';
+
+let test = require('tape');
+let anagram = require('./anagram.js')
+
+test('test anagramIfTrue', t => {
+    let actual = anagram("alma", "mala");
+    let expected = true;
+
+    t.equal(actual, expected);
+    t.end();
+});
