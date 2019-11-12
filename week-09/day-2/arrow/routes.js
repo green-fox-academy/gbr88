52622297917732616f1082ef8a36c86ccc9bb476
Csete Gábor <csgabor88@gmail.com>
2019-11-12 16:02:45 +0100

    error handling for <= 0 values and parseFloat and toFixed for correct display

--- week-09/day-2/arrow/routes.js
+++ week-09/day-2/arrow/routes.js
@@ -7,11 +7,11 @@ app.get('/yondu', (req, res) => {
     let distance = req.query.distance;
     let time = req.query.time;
     let speed = distance / time;
-    if (distance || time) {
+    if (distance && time > 0) {
         res.json({
-            "distance": parseInt(distance),
-            "time": parseInt(time),
-            "speed": speed
+            "distance": parseFloat(parseFloat(distance).toFixed(1)),
+            "time": parseFloat(parseFloat(time).toFixed(1)),
+            "speed": parseFloat(parseFloat(speed).toFixed(1))
         });
         res.status(200);
     } else {
